1ed454111f14d6c1b2d13066080c5116ac6f5622
r8dd1t <r8ddit@gmail.com>
2023-11-02 13:27:05 +0200

    Main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
     </div>
   );
 }
