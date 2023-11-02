90c0a47a5081d237bdf6a74f8bc95e202ea9588d
r8dd1t <r8ddit@gmail.com>
2023-11-02 14:36:39 +0200

    Display section

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DisplaySection = () => {
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant.</p>
+      <span className="description">
+        Display 2x brighter, battery 2x lower!
+      </span>
+      <button className="button">Try it!</button>
+      <button className="back-button">TOP</button>
+    </div>
+  );
+};
+
+export default DisplaySection;
